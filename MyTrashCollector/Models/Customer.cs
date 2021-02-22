1687360ad524de1eae78ae80f367386651284a41
Dustin <adustin70@gmail.com>
2021-02-22 11:25:06 -0500

    Added Customer model with properties including foreign key

---
+++ MyTrashCollector/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public double ZipCode { get; set; }
+        public DateTime PickUpDay { get; set; }
+        public bool ExtraPickUp { get; set; }
+        public double Balance { get; set; }
+        public DateTime StartDay { get; set; }
+        public DateTime EndaDay { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
