6dd9f0701271a8db0c298665ab84d1273e2dbcc7
Dustin <adustin70@gmail.com>
2021-02-23 15:34:09 -0500

    Added Employee model and EmployeesController

---
+++ MyTrashCollector/Controllers/EmployeesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyTrashCollector.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MyTrashCollector.Controllers
+{
+    [Authorize(Roles = "Employee")]
+    public class EmployeesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var customer = _context.Customer.Where(c => c.IdentityUserId == userId).ToList();
+            return View(customer);
+        }
+    }
+}
