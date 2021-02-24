1ecec8fece60fc705669a295e28dbbf33a5c812b
Dustin <adustin70@gmail.com>
2021-02-24 13:37:13 -0500

    Added Details method to EmployeesController and fixed Index method to display correct info. Added Details view to Employees view folder

--- MyTrashCollector/Controllers/EmployeesController.cs
+++ MyTrashCollector/Controllers/EmployeesController.cs
@@ -21,9 +21,10 @@ namespace MyTrashCollector.Controllers
 
         public IActionResult Index()
         {
+            var date = DateTime.Today.Date;
             var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
             var employee = _context.Employee.Where(e => e.IdentityUserId == userId).FirstOrDefault();
-            var trashZip = _context.Customer.Where(c => c.ZipCode == employee.ZipCode).Where(c => c.PickUpDay == DateTime.Today);
+            var trashZip = _context.Customer.Where(c => c.ZipCode == employee.ZipCode).Where(c => c.PickUpDay.Value.Date == date);
 
             if (employee == null)
             {
@@ -49,6 +50,16 @@ namespace MyTrashCollector.Controllers
             _context.Add(employee);
             _context.SaveChanges();
             return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details()
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var employee = _context.Employee.Where(e => e.IdentityUserId == userId).FirstOrDefault();
+            var trashZip = _context.Customer.Where(c => c.ZipCode == employee.ZipCode);
+            var date = DateTime.Now;
+            var pickup = _context.Customer.Where(c => c.PickUpDay.Value.Date.ToString("ddd") == date.ToString("ddd"));
+            return View(pickup);
         }
     }
 }
